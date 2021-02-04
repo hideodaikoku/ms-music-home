8eeca74cde5edbeee33902266640337ef3f8cc40
Hideo Daikoku <neildaikoku@gmail.com>
2021-02-05 04:37:33 +0900

    remove sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,6 @@ module.exports = {
     // },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     "gatsby-plugin-offline",
     {
       resolve: "gatsby-plugin-manifest",
