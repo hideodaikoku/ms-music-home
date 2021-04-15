fc8f3968f781bb5b2c5e10f94251dc80090e25a8
Leonard Choo <leonardchoo@esuefushinoMacBook-Pro.local>
2021-04-15 16:24:49 +0900

    still figuring out how to load thumbnail

--- src/components/article.js
+++ src/components/article.js
@@ -1,22 +1,53 @@
 import React from "react";
 import Img from "gatsby-image";
 import articleStyles from "../styles/components/article.module.scss";
+import { useStaticQuery, graphql } from "gatsby";
 import Thumbnail from  "../components/thumbnail";
 
+// function renderImage(file) {
+//   return (
+//     <Img fluid={file.node.childImageSharp.fluid} />
+//   )
+// }
+
 const Article = (props) => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     a: file(relativePath: { eq:  }) {
-  //       childImageSharp {
-  //         fluid(maxWidth: 600) {
-  //           ...GatsbyImageSharpFluid
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
+  // fetch all uploaded images being used as thumbnails
+  const data = useStaticQuery(graphql`
+    query {
+      images: allFile(filter: { sourceInstanceName: { eq: "uploaded-images" } }) {
+        edges {
+          node {
+            extension
+            relativePath
+            childImageSharp {
+              fluid(maxWidth: 600) {
+                base64
+              }
+            }
+          }
+        }
+      }
+    }
+`);
+
+  // loop thru the images, compare the title with props.thumbnail to display the correct one
+  // loop thru the images
+  var images = data.images.edges
+  for (var i=0; i < images.length; i++){
+    console.log("RELATIVE PATH", images[i].node.relativePath)
+    // if [i] equals the props.thumbnail
+    if ("/images/uploads/"+images[i].node.relativePath == props.thumbnail){
+      
+    }
+  }
+
+  
+  
+  // render the image of [i]th image
+
   const index = props.index;
   console.log(props.thumbnail)
+  
   return (
     <div className={articleStyles.article}>
       <div className={articleStyles.articleImage}>
@@ -24,8 +55,7 @@ const Article = (props) => {
           fluid={props.thumbnail}
           className={articleStyles.image}
         /> */}
-        <Thumbnail path={props.thumbnail} />
-        
+        {/* <Thumbnail path={props.thumbnail} /> */}
       </div>
       <div className={articleStyles.articleText}>
         <h1 className={articleStyles.articleTitle}>{props.title}</h1>
--- src/components/thumbnail.js
+++ src/components/thumbnail.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import Img from 'gatsby-image';
-import { StaticQuery, graphql } from 'gatsby';
+// import React from 'react';
+// import Img from 'gatsby-image';
+// import { StaticQuery, graphql } from 'gatsby';
 
-function renderImage(file) {
-  return (
-    <Img fluid={file.node.childImageSharp.fluid} />
-  )
-}
+// function renderImage(file) {
+//   return (
+//     <Img fluid={file.node.childImageSharp.fluid} />
+//   )
+// }
 
-const thumbnail = function (props) {
+// const thumbnail = function (props) {
 
-  return <StaticQuery
-    query={graphql`
-    query {
-      images: allFile(filter: { sourceInstanceName: { eq: "uploaded-images" } }) {
-        edges {
-          node {
-            extension 
-            relativePath
-            childImageSharp {
-            fluid(maxWidth: 600) {
-              base64
-            }
-          }
-        }
-      }
-    }
-  }
-    `}
-    render={(data) => {
-      const image = data.images.edges.find(
-        image => image.node.relativePath === props.path
-      )
-      return(renderImage(image))
-    }}
-  />
-}
+//   return <StaticQuery
+//     query={graphql`
+//     query {
+//       images: allFile(filter: { sourceInstanceName: { eq: "uploaded-images" } }) {
+//         edges {
+//           node {
+//             extension 
+//             relativePath
+//             childImageSharp {
+//             fluid(maxWidth: 600) {
+//               base64
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+//     `}
+//     render={(data) => {
+//       const image = data.images.edges.find(
+//         image => image.node.relativePath === props.path
+//       )
+//       return(renderImage(image))
+//     }}
+//   />
+// }
 
-export default thumbnail;
+// export default thumbnail;
