fabff94de713837e55c5daa55c27d148d850321a
Leonard Choo <leonardchoo@esuefushinoMacBook-Pro.local>
2021-06-07 19:18:36 +0900

    top page font, size, color changes

--- src/components/top.js
+++ src/components/top.js
@@ -10,24 +10,27 @@ const TopComponent = () => {
         <h4 className={landingStyles.subtext}>
           2050年の社会における音楽芸術はどのようなものでしょうか
         </h4>
-        <p className={landingStyles.desc}>
-          2050年 芸術科学立国を成し遂げ
-          <br />
-          ムーンショット音楽芸術を地球全体で共創します
-        </p>
-        <Link to="/message" className={landingStyles.link}>
-          [ READ MORE
-          <svg
-            width="63"
-            height="18"
-            viewBox="0 0 63 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M0 17H60L47.234 1" stroke="#015C99" strokeWidth="2" />
-          </svg>
-          ]
-        </Link>
+        <div className={landingStyles.desc}>
+          2050年
+          芸術科学立国を成し遂げムーンショット音楽芸術を地球全体で共創します
+        </div>
+        <div className={landingStyles.link}>
+          <Link to="/message">
+            <span>
+              [ READ MORE
+              <svg
+                width="63"
+                height="18"
+                viewBox="0 0 63 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0 17H60L47.234 1" stroke="#015C99" strokeWidth="2" />
+              </svg>
+              ]
+            </span>
+          </Link>
+        </div>
       </div>
     </div>
   );
